60b523ce1310b3ac9457fff8e950edfd2017c365
Shreyas kale <71918814+shreyaskale11@users.noreply.github.com>
2021-02-09 03:06:09 +0530

    sdsa

--- bcd.vhd
+++ bcd.vhd
@@ -19,7 +19,8 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use IEEE.std_logic_unsigned.all;
+use IEEE.NUMERIC_STD.all;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -31,40 +32,73 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity bcd is
 Port (
-BCDin : in STD_LOGIC_VECTOR (3 downto 0);  -- Slide Switch
-digit : out STD_LOGIC_VECTOR (3 downto 0);        -- Enable 4 digit
-Seven_Segment : out STD_LOGIC_VECTOR (7 downto 0)); -- 7 Segments and Dot LEDs
+BCDin0 : in STD_LOGIC_VECTOR (3 downto 0);
+BCDin1 : in STD_LOGIC_VECTOR (3 downto 0);  -- Slide Switch
+BCDin2 : in STD_LOGIC_VECTOR (3 downto 0);
+BCDin3 : in STD_LOGIC_VECTOR (3 downto 0);
+--i : in std_logic;
+reset: IN STD_LOGIC;
+clk: IN STD_LOGIC;
+sel: out STD_LOGIC;
+digit : out STD_LOGIC_VECTOR (3 downto 0);  -- Enable 4 digit
+Seven_Segment : out STD_ULOGIC_VECTOR (7 downto 0)); -- 7 Segments and Dot LEDs
 end bcd;
 
 architecture Behavioral of bcd is
 
+signal counter :  unsigned(15 downto 0) := (others =>'0');
+
+TYPE State_type is (A, B, C, D);  -- Define the states
+SIGNAL State : State_Type :=A;    -- Create a signal that uses 
+						      -- the different states
+type array44 is array(0 to 3)of std_logic_vector(3 downto 0);
+type array47 is array(0 to 3)of std_ulogic_vector(7 downto 0);
+signal number :  array44 := (others =>(others =>'0'));
+signal segment :  array47 := (others =>(others =>'0'));
+
 begin
-digit <= "1111"; 
-process(BCDin) begin
-case BCDin is
-when "0000" =>
-Seven_Segment <= "00000011"; --0
-when "0001" =>
-Seven_Segment <= "10011111"; --1
-when "0010" =>
-Seven_Segment <= "00100101"; --2
-when "0011" =>
-Seven_Segment <= "00001101"; --3
-when "0100" =>
-Seven_Segment <= "10011001"; --4
-when "0101" =>
-Seven_Segment <= "01001001"; --5
-when "0110" =>
-Seven_Segment <= "01000001"; --6
-when "0111" =>
-Seven_Segment <= "00011111"; --7
-when "1000" =>
-Seven_Segment <= "00000001"; --8
-when "1001" =>
-Seven_Segment <= "00001001"; --9
-when others =>
-Seven_Segment <= "11111111"; --null
-end case;
-end process;
+--digit <= "1111"; 
+number(0) <= BCDin0;
+number(1) <= BCDin1;
+number(2) <= BCDin2;
+number(3) <= BCDin3;
+translate_loop: for I in 0 to 3 generate
+begin
+	with number(I) select segment(I) <=
+		"00000011" when "0000",
+		"10011111"when "0001",
+		"00100101"when "0010",
+		"00001101"when "0011",
+		"10011001"when "0100",
+		"01001001"when "0101",
+		"01000001"when "0110",
+		"00011111"when "0111",
+		"00000001"when "1000",
+		"00001001"when "1001",
+		"11111111"when others;
+end generate;
+Seven_Segment(0) <= segment(0);
+	process(clk) begin
+--State <= A;
+		if (rising_edge(clk)) then
+			if (reset = '1') then
+				counter<= (others =>'0');
+			else
+				if(counter = 50_000) then
+					counter <= (others => '0');
+					case State is
+						when A => State <= B;
+						when B => State <= C;
+						when C => State <= D;
+						when D => State <= A;
+						when others => State <= A;
+					end case;
+				else
+					counter <= counter + 1;
+				end if;
+			end if;
+		end if;
+	end process;
+	
 end Behavioral;
 
